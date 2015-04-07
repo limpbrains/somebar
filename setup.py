67bee64eb44dbe26aa7e11d2d5acfc9fa3ac9c11
Ivan <ivan.vershigora@gmail.com>
2015-04-07 16:34:41 +0300

    Initial release

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+from os.path import join, dirname
+
+
+setup(
+    name = 'somebar',
+    scripts = ['somebar'],
+    version = '0.0.1',
+    license = 'MIT License',
+    description = 'Simple taskbar widget that displays color dot or custom icon',
+    author = 'Ivan',
+    author_email = 'ivan.vershigora@gmail.com',
+    url = 'https://github.com/limpbrains/somebar',
+    download_url  ='https://github.com/limpbrains/somebar/tarball/0.0.1',
+    keywords = ['AnyBar', 'somebar', 'taskbar', 'indicator'],
+    packages = find_packages(),
+    long_description = open(join(dirname(__file__), 'README.md')).read(),
+    classifiers = [
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Operating System :: Linux',
+    ],
+    package_data = {'somebar_icons' : ['*.png']},
+    install_requires = [
+        # 'gi'
+    ],
+)
