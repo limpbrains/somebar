06aa5a91a871ddb49a1a96493d722a468915fe35
Ivan <ivan.vershigora@gmail.com>
2015-04-07 16:49:36 +0300

    remove long_description

--- setup.py
+++ setup.py
@@ -5,16 +5,15 @@ from os.path import join, dirname
 setup(
     name = 'somebar',
     scripts = ['somebar'],
-    version = '0.0.2',
+    version = '0.0.3',
     license = 'MIT License',
     description = 'Simple taskbar widget that displays color dot or custom icon',
     author = 'Ivan',
     author_email = 'ivan.vershigora@gmail.com',
     url = 'https://github.com/limpbrains/somebar',
-    download_url  ='https://github.com/limpbrains/somebar/tarball/0.0.2',
+    download_url  ='https://github.com/limpbrains/somebar/tarball/0.0.3',
     keywords = ['AnyBar', 'somebar', 'taskbar', 'indicator'],
     packages = find_packages(),
-    long_description = open(join(dirname(__file__), 'README.md')).read(),
     classifiers = [
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
